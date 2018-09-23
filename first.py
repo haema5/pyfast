9cf11c95b7934a13613a13fb848fe363789e95c0
haema5 <killy.ny@gmail.com>
2018-09-23 20:24:11 +0300

    Урок 3. Числа; подключение модулей, функции справки

--- first.py
+++ first.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import os
+import sys
 import psutil
 
 #
@@ -15,16 +16,32 @@ answer = input("Хочешь поработать? (Y/N)")
 if answer == 'Y':
     print("Отлично, хозяин!")
     print("Я умею:")
-    print("  1 - вывести список файлов;")
-    print("  2 - вывести информацию о системе.")
+    print("  1 - вывести список файлов")
+    print("  2 - имя текущей директории")
+    print("  3 - вывести список процессов")
+    print("  4 - платформа (ОС)")
+    print("  5 - кодировка")
+    print("  6 - логин")
+    print("  7 - количество ЦПУ")
     do = int(input("Что сделать? "))
 
     if do == 1:
         print(os.listdir())
     elif do == 2:
-        psutil.pids()
+        print(os.getcwd())
+    elif do == 3:
+        print(psutil.pids())
+    elif do == 4:
+        print(sys.platform)
+    elif do == 5:
+        print(sys.getfilesystemencoding())
+    elif do == 6:
+        print(os.getlogin())
+    elif do == 7:
+        print(psutil.cpu_count())
     else:
-        pass
+        print("I don't know...")
+
 elif answer == 'N':
     print("Goodbye!")
 else:
