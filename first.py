6131a144412060e2c3b872e1ab71d012341c892d
haema5 <killy.ny@gmail.com>
2018-09-23 19:42:04 +0300

    2 урок

--- first.py
+++ first.py
@@ -1,2 +1,16 @@
-print("hello world")
-print("hello you")
+# coding: utf-8
+#
+
+print("Hello world")
+name = input("Your name: ")
+
+print(name, ", hello!")
+
+answer = input("хочешь поработать?")
+
+if answer == 'Y':
+    print("Super!")
+elif answer == 'N':
+    print("Goodbye...")
+else:
+    print("I don't know...")
