f14d31adf51c6c30163523cb3910cb04a870616e
haema5 <killy.ny@gmail.com>
2018-09-25 21:15:13 +0300

    add 2 features

--- first.py
+++ first.py
@@ -22,6 +22,8 @@ while answer != 'q':
         print("  2 - вывести информацию о системе")
         print("  3 - вывести список процессов")
         print("  4 - продублировать файлы")
+        print("  5 - дублировать определенный файл")
+        print("  6 - удалить файлы .dupl")
         do = int(input("Что сделать? "))
         if do == 1:
             print(os.listdir())
@@ -47,23 +49,26 @@ while answer != 'q':
                         newfile = file_list[i] + '.dupl'
                         shutil.copy(file_list[i], newfile)
                 i += 1
-        elif do == 4:
-            print("Дублируем файлы")
-            file_list = os.listdir()
+        elif do == 5:
             print(os.listdir())
-            # duplicate all files
+            file_name = input("Какой файл дублировать?")
+            if os.path.isfile(file_name):
+                newfile = file_name + '.dupl'
+                shutil.copy(file_name, newfile)
+        elif do == 6:
+            dir_name = input('Укажите имя директории: ')
+            file_list = os.listdir(dir_name)
             print(file_list)
             i = 0
             while i < len(file_list):
-                print(file_list[i])
-                if os.path.isfile(file_list[i]):
-                    if file_list[i] != '.git':
-                        newfile = file_list[i] + '.dupl'
-                        shutil.copy(file_list[i], newfile)
+                longname = os.path.join(dir_name, file_list[i])
+                if longname.endswith('.dupl'):
+                    print('Удаляю: ' + longname)
+                    os.remove(longname)
                 i += 1
+            print('Все файлы .dupl в директории ' + dir_name + ' удалены!')
         else:
             pass
-
 
     elif answer == 'n':
         print("Goodbye!")
